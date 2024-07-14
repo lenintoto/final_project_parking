a2a7e1aeb04dac78664a8d1e59924bfe173bfbde
Steven <sirpepega777@gmail.com>
2024-07-13 21:32:16 -0500

    septimo commit

---
+++ src/middlewares/autenticacion.js
@@ -0,0 +1,28 @@
+import jwt from "jsonwebtoken"
+import Usuarios from "../models/usuarios.js"
+import dotenv from "dotenv"
+dotenv.config()
+
+const autenticar = async(req, res, next)=>{
+
+    const token = req.headers.authorization
+    if(!token) return res.status(404).json({
+        msg: "Lo sentimos primero debe proporcionar un token"
+    })
+
+    const {authorization} = req.headers
+
+    try {
+        const {id, rol} = jwt.verify(authorization.split(" ")[1], process.env.JWT_SECRET)
+        if(rol === "usuario"){
+            req.usuario = await Usuarios.findById(id).lean().select("-password")
+            next()
+        }
+
+    } catch (error) {
+        const e = new Error("Formato del token no válido")
+        return res.status(404).json({msg: e.message})
+    }
+}
+
+export default autenticar
--- src/models/parqueaderos.js
+++ src/models/parqueaderos.js
@@ -1,14 +1,37 @@
 import mongoose from "mongoose"
 
 const parqueaderoSchema = new mongoose.Schema({
-    numero: {type: Number, require: true},
-    bloque : {type: String, require: true},
-    tipo: {type: String, require: true},
-    disponibilidad: {type: Boolean, require: true},
-    dimensiones: {type: String, require: true},
-    reservado: {type: Boolean, require: false, default: false},
-    status: {type: Boolean, require: true, default: true}
-},{
+    numero: {
+        type: Number,
+        require: true
+    },
+    bloque: {
+        type: String,
+        require: true
+    },
+    tipo: {
+        type: String,
+        require: true
+    },
+    disponibilidad: {
+        type: Boolean,
+        require: true
+    },
+    dimensiones: {
+        type: String,
+        require: true
+    },
+    reservado: {
+        type: Boolean,
+        require: false,
+        default: false
+    },
+    status: {
+        type: Boolean,
+        require: true,
+        default: true
+    }
+}, {
     timestamps: true
 })
 
