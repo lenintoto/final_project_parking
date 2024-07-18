d30dbf728592a98f09cc176ca14746252ade4995
Lenin Gómez <133397574+lenintoto@users.noreply.github.com>
2024-07-17 19:22:09 -0500

    Update swaggerOptions.js

--- src/swaggerOptions.js
+++ src/swaggerOptions.js
@@ -8,7 +8,7 @@ export  const options = {
         },
         servers: [
             {
-                url: "http://localhost:3000", // URL del servidor
+                url: "http://localhost:3000/api", // URL del servidor
             },
         ],
     },
